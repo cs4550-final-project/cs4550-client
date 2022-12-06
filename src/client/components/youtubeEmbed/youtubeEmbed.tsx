823478a3249614929fd0857ca1078299319a7029
britneyart80 <britneychen@Britneys-MacBook-Pro.local>
2022-12-06 17:04:26 -0500

    youtube embed component

---
+++ src/client/components/youtubeEmbed/youtubeEmbed.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./youtubeEmbed.module.scss";
+
+type YoutubeEmbedProps = {
+  embedId: string;
+};
+
+const YoutubeEmbed = ({ embedId }: YoutubeEmbedProps) => (
+  <div className={styles.videoResponsive}>
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired,
+};
+
+export default YoutubeEmbed;
